cd7c1731524f0391e1e3d678cb0a2593f78bd857
João Victor <galvaopp@gmail.com>
2023-06-14 22:49:25 -0300

    feat: create sidebar (#2)

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,23 @@
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 import { Header } from "./components/Header"
+
+import styles from './App.module.css';
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
 
   return (
     <>
       <Header />
-        <Post
-          author="João"
-          content="Olá, mundo!"
-        />
+       <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post author="Joao" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum." />
+          <Post author="Joao" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum." />
+        </main>
+      </div>
     </>
   )
 }
