8db8ed547df50cecdfbf4c641ef5ff42ed9b80f5
João Victor <galvaopp@gmail.com>
2023-06-14 23:44:17 -0300

    feat/posts (#3)
    * feat: create new posts
    
    * feat: create component avatar
    
    * fix: align columns

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/10669905?v=4" />
+        <Avatar src="https://github.com/galvaodev.png" />
 
         <strong>Joao</strong>
         <span>Web Developer</span>
